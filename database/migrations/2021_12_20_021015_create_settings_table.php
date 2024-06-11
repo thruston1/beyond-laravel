989d5618857ad7ebf787e9c431cb1d3804bf44d7
angga <anggayudha764@gmail.com>
2024-06-11 08:53:14 +0700

    add migration

---
+++ database/migrations/2021_12_20_021015_create_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('key')->nullable();
+            $table->longText('value')->nullable();
+            $table->string('type')->default('text')->nullable();
+            $table->index(['id', 'key'], 'setting_in');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
---
+++ database/migrations/2024_04_19_085624_create_table_product.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_product', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id')->default(0);
+            $table->string('name')->nullable();
+            $table->decimal('price', 26, 2)->default(0);
+            $table->decimal('price_sale', 26, 2)->default(0);
+            $table->integer('qty')->default(0);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_product');
+    }
+};
